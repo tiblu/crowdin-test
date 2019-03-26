57c17fc4bcd83b8c1e1057b0a7d1dbbeceed8414
Mikk Andresen <mikk.andresen@gmail.com>
2019-03-26 15:07:41 +0200

    src/index.js - modify trasnlsations AND src/index.js after Crowdin versions setup in feature-2-translate branch

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@
 
 console.log('Hello, how are you today?');
 console.log('Modified the non localisation related file for fun before Crowdin translation sync! 1');
-console.log('Modified the non localisation related file for before versions setup! 2');
+console.log('Modified the non localisation related file for before versions setup! 3');
+console.log('Modified the non localisation related file after versions setup, code modified in feature-2-translate branch only');
