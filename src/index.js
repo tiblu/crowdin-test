007521fbcb8bd01c6cb814932275f56e18cdb700
Mikk Andresen <mikk.andresen@gmail.com>
2019-03-28 21:58:14 +0200

    src/index.js - modify trasnlsations AND src/index.js after Crowdin versions setup in feature-3-translate branch to verify the PR bugfix

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@
 
 console.log('Hello, how are you today?');
 console.log('Modified the non localisation related file for fun before Crowdin translation sync! 1');
-console.log('Modified the non localisation related file for before versions setup! 3');
+console.log('Modified the non localisation related file for before versions setup! 4');
 console.log('Modified the non localisation related file in master to verify the PR bugfix by Crowdin');
+console.log('Modified the non localisation related file in feature-3-translate branch to verify the PR bugfix by Crowdin');
